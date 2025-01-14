d2bb878bc511226fb28868b75c9a0ba9bddcb418
Admin <jainmintu557@gmail.com>
2025-01-14 18:29:08 +0530

    new files

---
+++ src/test/java/com/shashankjaincompany/ex_selenium_xpath/Taskidrive360.java
@@ -0,0 +1,41 @@
+package com.shashankjaincompany.ex_selenium_xpath;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Taskidrive360
+{
+
+   @Test
+    public void idrivelogin() throws InterruptedException {
+        WebDriver driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.idrive360.com/enterprise/login");
+
+       WebElement email = driver.findElement(By.xpath("//input[@id='username']"));
+       email.sendKeys("augtest_040823@idrive.com");
+
+       WebElement password = driver.findElement(By.xpath("//input[@id='password']"));
+       password.sendKeys("123456");
+
+
+       WebElement signin = driver.findElement(By.xpath("//button[@id='frm-btn']"));
+       signin.click();
+
+       Thread.sleep(12000);
+
+       WebElement message = driver.findElement(By.xpath("//h5[text()='Your free trial has expired']"));
+       Assert.assertEquals(message.getText(),"Your free trial has expired");
+
+
+    }
+}
